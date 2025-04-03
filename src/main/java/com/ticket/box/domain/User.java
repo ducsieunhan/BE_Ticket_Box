866e2d7d406e8ec875f81ce536940d89eee9acf7
Vin Com <0plmzxc@gmail.com>
2025-04-03 18:46:30 +0700

    fix user crud and add validate

--- src/main/java/com/ticket/box/domain/User.java
+++ src/main/java/com/ticket/box/domain/User.java
@@ -16,12 +16,17 @@ public class User {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
+  @Email(message = "Invalid email format")
   private String email;
 
+  @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
   private String name;
 
+  @Size(min = 6, message = "Password must be at least 8 characters long")
   private String password;
 
+  @Pattern(regexp = "^[0-9]*$", message = "Phone number must contain only digits")
+  @Pattern(regexp = "^.{10,15}$", message = "Phone number must be 10-15 characters long")
   private String phone;
 
   private String dob;
