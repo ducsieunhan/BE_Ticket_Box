1aede4764326fcbba510b575ab2308751d24f270
Vin Com <0plmzxc@gmail.com>
2025-04-05 20:16:52 +0700

    add file handle

---
+++ src/main/java/com/ticket/box/domain/response/ResUploadFileDTO.java
@@ -0,0 +1,17 @@
+package com.ticket.box.domain.response;
+
+import java.time.Instant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResUploadFileDTO {
+    private String fileName;
+    private Instant uploadedAt;
+}
