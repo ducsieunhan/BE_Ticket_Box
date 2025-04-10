a8863c8b527ef238f3467367e8414b29daaa1f63
ducsieunhan <Chroller03@gmail.com>
2025-04-10 23:20:17 +0700

    add validation for req

--- src/main/java/com/ticket/box/domain/require/ReqOrderDetailDto.java
+++ src/main/java/com/ticket/box/domain/require/ReqOrderDetailDto.java
@@ -1,13 +1,22 @@
 package com.ticket.box.domain.require;
 
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ReqOrderDetailDto {
+
   private long orderId;
+
+  @Positive(message = "Ticket Id must be not null")
   private long ticketId;
+
+  @PositiveOrZero(message = "Sub total price must be positive ")
   private double subTotal;
+
+  @Positive(message = "Quantity must be positive")
   private long quantity;
 }
--- src/main/java/com/ticket/box/domain/require/ReqUpdateOrderDetail.java
+++ src/main/java/com/ticket/box/domain/require/ReqUpdateOrderDetail.java
@@ -1,13 +1,23 @@
 package com.ticket.box.domain.require;
 
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ReqUpdateOrderDetail {
+
+  @Positive(message = "Order detail id must be not null")
   private long orderDetailId;
+
+  @Positive(message = "Ticket Id must be not null")
   private long ticketId;
+
+  @PositiveOrZero(message = "Sub total price must be positive ")
   private double subTotal;
+
+  @Positive(message = "Quantity must be positive")
   private long quantity;
 }
