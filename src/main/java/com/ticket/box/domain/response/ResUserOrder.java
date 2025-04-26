f0109f2c6b9f1d8ee130a11ce0e9428f1b3f38a0
ducsieunhan <Chroller03@gmail.com>
2025-04-27 00:21:41 +0700

    update finale 1

--- src/main/java/com/ticket/box/domain/response/ResUserOrder.java
+++ src/main/java/com/ticket/box/domain/response/ResUserOrder.java
@@ -1,2 +1,39 @@
-package com.ticket.box.domain.response;public class ResUserOrder {
+package com.ticket.box.domain.response;
+
+import com.ticket.box.util.constant.StatusEventEnum;
+import com.ticket.box.util.constant.StatusOrderEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+public class ResUserOrder {
+  private long orderId;
+
+  private double totalPrice;
+
+  private StatusOrderEnum status;
+
+  private String receiverEmail;
+
+  private Instant createdAt;
+
+  private List<ResUserOrderDetail> items;
+
+  private EventDTO event;
+
+  @Getter
+  @Setter
+  public static class EventDTO {
+    private String name;
+    private String banner ;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private StatusEventEnum status ;
+  }
+
 }
--- src/main/java/com/ticket/box/domain/response/ResUserOrderDetail.java
+++ src/main/java/com/ticket/box/domain/response/ResUserOrderDetail.java
@@ -1,2 +1,26 @@
-package com.ticket.box.domain.response;public class ResUserOrderDetail {
+package com.ticket.box.domain.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+import java.time.Instant;
+
+@Getter
+@Setter
+public class ResUserOrderDetail {
+  private long orderDetailId;
+  private TicketOrder ticket;
+  private long quantity;
+  private double price;
+  private double subTotal;
+
+  @Getter
+  @Setter
+  @NoArgsConstructor
+  public static class TicketOrder {
+    private long ticketId;
+    private String name;
+  }
 }
